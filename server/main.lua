cff94283f4b6efb543f84d67548c36630260a347
PenguScripts <102926606+PenguScript@users.noreply.github.com>
2022-09-05 19:30:45 -0400

    Config Job Name + Patch

--- server/main.lua
+++ server/main.lua
@@ -9,7 +9,7 @@ AddEventHandler("qb-limeysjob:bill:player", function(playerId, amount)
         local biller = QBCore.Functions.GetPlayer(source)
         local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
         local amount = tonumber(amount)
-        if biller.PlayerData.job.name == 'limeys' then
+        if biller.PlayerData.job.name == Config.JobName then
             if billed ~= nil then
                 if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
